a0cee3787157c178f13afdbacfa63bbd0e7fc69c
braydonmillard <46946203+braydonmillard@users.noreply.github.com>
2022-07-14 11:59:29 -0400

    Business logic defined

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { AddNewItem } from './AddNewItem';
 import { Column } from './Column';
 import { AppContainer } from './styles';
+import { useAppState } from './state/AppStateContext';
+import { addList } from './state/actions';
 
 export const App = () => {
+  const { lists, dispatch } = useAppState();
+
   return (
     <AppContainer>
-      <Column text='Todo:' />
-      <AddNewItem toggleButtonText='+ Add another list' onAdd={console.log} />
+      {lists.map((list) => (
+        <Column text={list.text} key={list.id} id={list.id} />
+      ))}
+      <AddNewItem
+        toggleButtonText='+ Add another list'
+        onAdd={(text) => dispatch(addList(text))}
+      />
     </AppContainer>
   );
 };
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,7 +1,10 @@
+import React from 'react';
+
 import { CardContainer } from './styles';
 
 type CardProps = {
   text: string;
+  id: string;
 };
 
 export const Card = ({ text }: CardProps) => {
